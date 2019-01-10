decaa802025a9a377cfbb27bb2141ac3aba0bf0f
SARAH POLLNOW <SPOLLNOW@GSE.HARVARD.EDU>
2019-01-10 17:42:19 -0500

    try to add an interactive element with the fox... but I think my computer is malfunctioning

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var myHeading = document.querySelector('h1');
+myHeading.texContent = 'Hello World!';
+
+// Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
+
+ 
